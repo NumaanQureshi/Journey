ccf4e2f8dc4871d4cde1a1081c14c3411ca4cbae
Numaan <numaan1@hotmail.com>
2025-10-06 14:53:28 -0400

    Added widgets folder

---
+++ journey_application/lib/widgets/video_background.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoBackground extends StatefulWidget {
+  const VideoBackground({super.key, required this.asset});
+
+  final String asset;
+
+  @override
+  State<VideoBackground> createState() => _VideoBackgroundState();
+}
+
+class _VideoBackgroundState extends State<VideoBackground> {
+  late final VideoPlayerController _controller;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _controller = VideoPlayerController.asset(widget.asset)
+  //     ..setLooping(true)
+  //     ..setVolume(0)
+  //     ..initialize().then((_) {
+  //       setState(() {});
+  //       _controller.play();
+  //     });
+  // }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_controller.value.isInitialized) return const SizedBox.shrink();
+    return SizedBox.expand(
+      child: FittedBox(
+        fit: BoxFit.cover,
+        clipBehavior: Clip.hardEdge,
+        child: SizedBox(
+          width: _controller.value.size.width,
+          height: _controller.value.size.height,
+          child: VideoPlayer(_controller),
+        ),
+      ),
+    );
+  }
+}
