707e4a5f615109552ec7924a0004c35b46084fc6
Numaan <numaan1@hotmail.com>
2025-10-06 14:57:28 -0400

    Added plugins

--- journey_application/macos/Flutter/GeneratedPluginRegistrant.swift
+++ journey_application/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import google_sign_in_ios
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
