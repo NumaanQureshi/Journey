daa3d17d70e43da003df105032f92e58fe9f8e7f
NumaanAce <numaan1@hotmail.com>
2025-11-02 18:18:43 -0500

    style: changing workout screen

--- lib/screens/workout_screen.dart
+++ lib/screens/workout_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'side_menu.dart';
+import 'dart:core'; 
+// import 'dart:async';
 
 class Workout extends StatelessWidget {
   const Workout({super.key});
 
-  void _notImplemented(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Not Implemented')),
-    );
-  }
+  // void _notImplemented(BuildContext context) {
+  //   ScaffoldMessenger.of(context).showSnackBar(
+  //     const SnackBar(content: Text('Not Implemented')),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -36,61 +38,37 @@ class Workout extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          Positioned(
+            left: 20,
+            bottom: 10,
+            child: Center(
               child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      _buildCircularIconButton(
-                        context,
-                        icon: Icons.timer_outlined,
-                        tooltip: 'Timer',
-                        onPressed: () => _notImplemented(context),
-                        color: const Color(0xFFBF6A02),
-                      ),
-                      _buildCircularIconButton(
-                        context,
-                        icon: Icons.history,
-                        tooltip: 'Sessions',
-                        onPressed: () => _notImplemented(context),
-                        color: Colors.blue,
-                      ),
-                      _buildCircularIconButton(
-                        context,
-                        icon: Icons.list_alt,
-                        tooltip: 'Plans',
-                        onPressed: () => _notImplemented(context),
-                        color: const Color(0xFF667DB5),
-                      ),
-                    ],
-                  ),
+                  Text('Test 1'),
+                  Text('Test 2'),
+                  Text('Test 3'),
                 ],
-              ),
-            ),
-          ),
+              )
+            ) 
+          )
         ],
       ),
     );
   }
-
-  Widget _buildCircularIconButton(BuildContext context, {required IconData icon, required String tooltip, required VoidCallback onPressed, required Color color}) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color,
-        shape: BoxShape.circle,
-      ),
-      child: IconButton(
-        icon: Icon(icon),
-        iconSize: 48,
-        color: Colors.white,
-        onPressed: onPressed,
-        tooltip: tooltip,
-      ),
-    );
-  }
 }
+//   Widget _buildCircularIconButton(BuildContext context, {required IconData icon, required String tooltip, required VoidCallback onPressed, required Color color}) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         color: color,
+//         shape: BoxShape.circle,
+//       ),
+//       child: IconButton(
+//         icon: Icon(icon),
+//         iconSize: 48,
+//         color: Colors.white,
+//         onPressed: onPressed,
+//         tooltip: tooltip,
+//       ),
+//     );
+//   }
+// }
