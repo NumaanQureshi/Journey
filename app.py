c89e955c20e8fcb8a4846a397afc5b4e3440e94e
NumaanAce <numaan1@hotmail.com>
2025-11-04 18:53:15 -0500

    style: cleaned out commented code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
 import os
-from flask_cors import CORS     # CORS import needed to test app locally
+from flask_cors import CORS     # CORS needed to test app locally
 import bcrypt
 import jwt                      # Encode / Decode
 import datetime
@@ -40,6 +40,7 @@ def token_required(f):
 
     return decorated
 
+# DB connection
 def get_db_connection():
     conn = psycopg2.connect(DATABASE_URL)
     return conn
@@ -61,13 +62,13 @@ def test_db():
         return jsonify({"success": True, "database": db_version[0]})
     except Exception as e:
         return jsonify({"success": False, "error": str(e)})
+    
 # GET all users
 @app.route('/api/users', methods=['GET'])
 def get_users():
     try:
         conn = get_db_connection()
         cur = conn.cursor()
-        # cur.execute('SELECT id, username, email, age, gender, height_in, weight_lb, created_at FROM users;')
         cur.execute('SELECT id, email, age, gender, height_in, weight_lb, created_at FROM users;')
         users = cur.fetchall()
         cur.close()
@@ -82,7 +83,6 @@ def get_user(user_id):
     try:
         conn = get_db_connection()
         cur = conn.cursor()
-        # cur.execute('SELECT id, username, email, age, gender, height_in, weight_lb, created_at FROM users WHERE id = %s;', (user_id,))
         cur.execute('SELECT id, email, age, gender, height_in, weight_lb, created_at FROM users WHERE id = %s;', (user_id,))        
         user = cur.fetchone()
         cur.close()
@@ -94,21 +94,19 @@ def get_user(user_id):
     except Exception as e:
         return jsonify({"success": False, "error": str(e)})
 
-# CREATE new user
+# CREATE new user (sign up)
 @app.route('/api/auth/register', methods=['POST'])
 def register():
     conn = None
     cur = None
     try:
         user_login_info = request.get_json()
-        # if not user_login_info.get('username'):
-        #     return jsonify({'success': False, 'error': 'Username is required'}), 400
         if not user_login_info.get('email'):
             return jsonify({'success': False, 'error': 'Email is required'}), 400
         if not user_login_info.get('password'):
             return jsonify({'success': False, 'error': 'Password is required'}), 400
         password = user_login_info['password']
-        if len(password) < 8: #For security purposes, we want a secure password that's at least 8 characters long
+        if len(password) < 8: # For security purposes, we want a secure password that's at least 8 characters long
             return jsonify({
                 'success': False,
                 'error': 'Password must be at least 8 characters long'
@@ -116,9 +114,6 @@ def register():
         password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
         conn = get_db_connection()
         cur = conn.cursor()
-        # cur.execute( """
-        #     INSERT INTO users (username, email, password_hash, age, gender, height_in, weight_lb) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id, username, email, age, gender, height_in, weight_lb, created_at""",
-        #     (user_login_info['username'],user_login_info['email'],password_hash,user_login_info.get('age'),user_login_info.get('gender'),user_login_info.get('height_in'),user_login_info.get('weight_lb')))
         cur.execute("""
             INSERT INTO users (email, password_hash, age, gender, height_in, weight_lb) 
             VALUES (%s, %s, %s, %s, %s, %s) 
@@ -149,14 +144,6 @@ def register():
             'success': True,
             'message': 'Registration successful',
             'user': {
-                # 'id': user[0],
-                # 'username': user[1],
-                # 'email': user[2],
-                # 'age': user[3],
-                # 'gender': user[4],
-                # 'height_in': user[5],
-                # 'weight_lb': user[6],
-                # 'created_at': str(user[7])
                 'id': user[0],
                 'email': user[1],
                 'age': user[3],
@@ -171,11 +158,6 @@ def register():
         if conn:
             conn.rollback()
         error_msg = str(e)
-        # if 'username' in error_msg:
-        #     return jsonify({
-        #         'success': False,
-        #         'error': 'Username already exists'
-        #     }), 400
         if 'email' in error_msg:
             return jsonify({
                 'success': False,
@@ -200,7 +182,7 @@ def register():
         if conn:
             conn.close()
 
-
+# UPDATING User Profile 
 @app.route('/api/auth/me', methods=['PUT'])
 @token_required
 def update_user(user_id):
@@ -243,14 +225,6 @@ def update_user(user_id):
             'success': True,
             'message': 'Profile updated successfully',
             'user': {
-                # 'id': user[0],
-                # 'username': user[1],
-                # 'email': user[2],
-                # 'age': user[3],
-                # 'gender': user[4],
-                # 'height_in': user[5],
-                # 'weight_lb': user[6],
-                # 'updated_at': str(user[7])
                 'id': user[0],
                 'email': user[1],
                 'age': user[3],
@@ -275,7 +249,7 @@ def update_user(user_id):
         if conn:
             conn.close()
 
-
+# UPDATING User password
 @app.route('/api/auth/me/password', methods=['PUT'])
 @token_required
 def update_password(user_id):
@@ -361,6 +335,7 @@ def update_password(user_id):
         if conn:
             conn.close()
 
+# LOGIN
 @app.route('/api/auth/login', methods=['POST'])
 def login():
     conn = None
@@ -400,8 +375,7 @@ def login():
                 'success': False,
                 'error': 'Invalid email or password'
             }), 401
-        # Verify password with bcrypt
-        # if not bcrypt.checkpw(password.encode('utf-8'), password_hash.encode('utf-8')):
+        # password verification with bcrypt
         if not bcrypt.checkpw(
             password.encode('utf-8'), 
             password_hash if isinstance(password_hash, bytes) else password_hash.encode('utf-8')
@@ -421,14 +395,6 @@ def login():
         )
         # Remove password_hash from response
         user_data = {
-            # 'id': user[0],
-            # 'username': user[1],
-            # 'email': user[2],
-            # 'age': user[4],
-            # 'gender': user[5],
-            # 'height_in': user[6],
-            # 'weight_lb': user[7],
-            # 'created_at': str(user[8])
             'id': user[0],
             'email': user[1],
             'age': user[3],
@@ -460,6 +426,7 @@ def login():
             cur.close()
         if conn:
             conn.close()
+
 # DELETE user
 @app.route('/api/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
