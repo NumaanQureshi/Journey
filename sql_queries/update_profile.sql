5e52bcaeb164fe6ca23bbac16f2fc29f718f663f
Numaan <numaan1@hotmail.com>
2025-11-13 13:59:31 -0500

    fix: added new varibles to update

--- sql_queries/update_profile.sql
+++ sql_queries/update_profile.sql
@@ -1,6 +1,10 @@
 UPDATE profiles
-SET age = COALESCE(%s, age),
+SET name = COALESCE(%s, name),
+    age = COALESCE(%s, age),
     gender = COALESCE(%s, gender),
     height_in = COALESCE(%s, height_in),
-    weight_lb = COALESCE(%s, weight_lb)
+    weight_lb = COALESCE(%s, weight_lb),
+    goal_weight_lb = COALESCE(%s, goal_weight_lb),
+    main_focus = COALESCE(%s, main_focus),
+    activity_intensity = COALESCE(%s, activity_intensity)
 WHERE user_id = %s
