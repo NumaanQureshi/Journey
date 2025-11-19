5aa7318803e96c50472e4f7aa9a5e8ff60b4f3f9
Numaan <numaan1@hotmail.com>
2025-11-18 23:10:40 -0500

    feat: added thememode to app start

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'screens/login_screen.dart';
+import 'providers/theme_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const Journey());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: const Journey(),
+    ),
+  );
 }
 
 class Journey extends StatelessWidget {
@@ -12,9 +19,22 @@ class Journey extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: const LoginScreen(),
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          title: 'Journey',
+          themeMode: themeProvider.themeMode,
+          // Define your light theme
+          theme: ThemeData(
+            brightness: Brightness.light,
+            primarySwatch: Colors.blue,
+            // You can customize other properties like fonts, etc.
+          ),
+          // Define your dark theme
+          darkTheme: ThemeData(brightness: Brightness.dark),
+          home: const LoginScreen(),
+        );
+      },
     );
   }
 }
