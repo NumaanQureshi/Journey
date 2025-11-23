173706d9fdef5ca499acc013db89fb540e156752
Numaan <numaan1@hotmail.com>
2025-11-23 03:00:24 -0500

    removed theme wrapper for design consistency

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'screens/login_screen.dart';
-import 'providers/theme_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => ThemeProvider(),
-      child: const Journey(),
-    ),
-  );
+  runApp(const Journey());
 }
 
 class Journey extends StatelessWidget {
@@ -19,22 +12,9 @@ class Journey extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeProvider>(
-      builder: (context, themeProvider, child) {
-        return MaterialApp(
-          title: 'Journey',
-          themeMode: themeProvider.themeMode,
-          // Define your light theme
-          theme: ThemeData(
-            brightness: Brightness.light,
-            primarySwatch: Colors.blue,
-            // You can customize other properties like fonts, etc.
-          ),
-          // Define your dark theme
-          darkTheme: ThemeData(brightness: Brightness.dark),
-          home: const LoginScreen(),
-        );
-      },
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: const LoginScreen(),
     );
   }
 }
