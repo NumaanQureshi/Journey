ff331522c44799c4d6ecb3d021c9c9205bd0d799
Numaan <numaan1@hotmail.com>
2025-11-27 04:07:43 -0500

    fix: changed debug user info

--- lib/featureflags/feature_flags.dart
+++ lib/featureflags/feature_flags.dart
@@ -2,16 +2,13 @@
 
 /// Controls whether the app bypasses the login flow and mocks authentication.
 /// 
-/// ⚠️ IMPORTANT: Must be set to `false` for production/release builds.
+/// ENSURE THIS IS SET TO FALSE IN PRODUCTION
 const bool kSkipAuthentication = false; // Set this to false before releasing!
 
-/// The ID of the test user to assume when kSkipAuthentication is true.
-/// This ID must match the 'DEBUG_USER_ID=5' set in your Flask backend's .env file.
-const int kDebugUserId = 5;
+/// DEBUG_ID = 14
+/// Must match the 'DEBUG_USER_ID=5' set in backend .env file.
+const int kDebugUserId = 4;
 
-/// The mock token to be used for API calls when kSkipAuthentication is true.
-/// This token doesn't need to be valid, as the Flask backend skips validation, 
-/// but a non-empty string is often needed for the 'Authorization' header to exist.
-const String kDebugAuthToken = 'MOCK_TOKEN_FOR_DEBUG_5'; 
+const String kDebugAuthToken = 'MOCK_TOKEN_FOR_DEBUG_4'; 
 
-// You can add other debug flags here later, like kSkipDatabaseCalls, etc.
+// OTHER FLAGS
