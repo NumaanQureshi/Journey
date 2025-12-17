36fc38b59be21a25ed6a79b039e575e4f1bdf75d
Numaan <numaan1@hotmail.com>
2025-12-16 23:40:48 -0500

    removed commented code

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -14,21 +14,6 @@ class SettingsScreen extends StatelessWidget {
       ),
     );
   }
-
-  // Route _createSlideRoute(Widget page) {
-  //   return PageRouteBuilder(
-  //     pageBuilder: (context, animation, secondaryAnimation) => page,
-  //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-  //       const begin = Offset(1.0, 0.0);
-  //       const end = Offset.zero;
-  //       final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: Curves.ease));
-  //       return SlideTransition(
-  //         position: animation.drive(tween),
-  //         child: child,
-  //       );
-  //     },
-  //   );
-  // }
   
   @override
   Widget build(BuildContext context) {
