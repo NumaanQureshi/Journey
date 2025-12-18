8ff2ab3f16f24c477da34e22b60ca4e07d7112d7
Numaan <numaan1@hotmail.com>
2025-12-18 01:44:50 -0500

    feat: added endpoint for workouts

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,11 +1,12 @@
 class ApiService {
   static String getBaseUrl() {
-    return 'https://journey-backend-zqz7.onrender.com/api';
+    return 'http://10.0.2.2:5000/api';
   }
 
   // You can add specific endpoint getters here if you like
   static String auth() => '${getBaseUrl()}/auth';
   static String challenges() => '${getBaseUrl()}/challenges/';
-  static String me() => '${auth()}/me';
+  static String me() => '${getBaseUrl()}/users/me';
   static String ai() => '${getBaseUrl()}/ai';
+  static String workouts() => '${getBaseUrl()}/workouts';
 }
