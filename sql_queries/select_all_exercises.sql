e070c0168172e5fd6e99094cd73e623fbcdf58c6
Numaan <numaan1@hotmail.com>
2025-12-18 13:03:54 -0500

    added exercise table queries

---
+++ sql_queries/select_all_exercises.sql
@@ -0,0 +1,16 @@
+SELECT 
+    id,
+    name,
+    description,
+    category,
+    difficulty_level,
+    mechanic,
+    equipment,
+    primary_muscles,
+    secondary_muscles,
+    instructions,
+    category_major,
+    images
+FROM exercises
+ORDER BY name
+LIMIT %s OFFSET %s;
