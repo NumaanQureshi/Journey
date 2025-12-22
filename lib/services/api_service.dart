a2e15a8fc20615b5ee03ae03c8d3f7cd7b406aa1
TawhidulIslam0 <147176853+TawhidulIslam0@users.noreply.github.com>
2025-12-22 18:43:00 -0500

    Update api_service.dart
    added workout endpoint

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -6,6 +6,7 @@ class ApiService {
   // You can add specific endpoint getters here if you like
   static String auth() => '${getBaseUrl()}/auth';
   static String challenges() => '${getBaseUrl()}/challenges/';
-  static String me() => '${auth()}/me';
+  static String me() => '${getBaseUrl()}/users/me';
   static String ai() => '${getBaseUrl()}/ai';
+  static String workouts() => '${getBaseUrl()}/workouts';
 }
